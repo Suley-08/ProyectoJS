037b2d4d04164c745a428a3bdc97c4dc8430ce87
Karen Rivera <karenriverah.18@gmail.com>
2022-11-16 16:09:17 -0500

    hola

---
+++ script.js
@@ -0,0 +1,30 @@
+//EJERCICIO 1
+/*
+let hombres = +prompt ("Ingrese el número de hombres")
+
+let mujeres = +prompt ("Ingrese el número de mujeres")
+
+estudiantes = (hombres+mujeres)
+
+alert(`El porcentaje de hombres es ${[(hombres/estudiantes)*100]} "%" `)
+alert(`El porcentaje de mujeres es ${[(mujeres/estudiantes)*100]} "%"`) */
+
+//EJERCICIO2
+
+/*
+let dinero = +prompt ("Ingrese el monto de inversion en soles")
+
+alert(`El monto invertido en la Av. La Mar es  ${(dinero*35/100)} soles`)
+alert(`El monto invertido en la Av. Abancay es  ${(dinero*25/100)} soles`)
+alert(`El monto invertido en la la Av. 28 de Julio es  ${(dinero*10/100)} soles`)
+alert(`El monto invertido en la Av. Tacna es  ${(dinero*15/100)} soles`)*/
+
+//EJERCICIO 3
+
+let entradageneral = +prompt ("Ingrese el numero de entradas generales vendidas")
+let entradaMayores = +prompt ("Ingrese el numero de entradas para mayores de 65 años vendidas")
+let entradaMenores = +prompt ("Ingrese el numero de entradas para menores de edad vendidas")
+
+ alert(`El numero de entradas generales son en total es  ${entradageneral*150} soles`)
+ alert(`El numero de entradas para mayores son en total es  ${entradaMayores*50} soles`)
+ alert(`El numero de entradas menores son en total es  ${entradaMenores*80} soles`)
